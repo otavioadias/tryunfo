3a64b94ff70854ace82918b35656043806a06453
Otávio Azevedo <otavioad1997@gmail.com>
2022-05-17 21:29:49 +0200

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -13,21 +13,60 @@ class App extends React.Component {
     cardRare: '',
     cardTrunfo: false,
     hasTrunfo: '',
+    isSaveButtonDisabled: true,
   };
+
+  validation = () => {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+    } = this.state;
+
+    const totalValueAttr = 210;
+    const maxValueAttr = 90;
+    const minValueAttr = 0;
+    const attr1 = parseFloat(cardAttr1);
+    const attr2 = parseFloat(cardAttr2);
+    const attr3 = parseFloat(cardAttr3);
+
+    if (cardName.length > minValueAttr
+      && cardDescription.length > minValueAttr
+      && attr1 >= minValueAttr
+      && attr1 <= maxValueAttr
+      && attr2 >= minValueAttr
+      && attr2 <= maxValueAttr
+      && attr3 >= minValueAttr
+      && attr3 <= maxValueAttr
+      && cardImage.length > minValueAttr
+      && ((attr1 + attr2 + attr3) <= totalValueAttr)
+    ) {
+      return this.setState({ isSaveButtonDisabled: false });
+    }
+    this.setState({ isSaveButtonDisabled: true });
+  }
 
   onInputChange = (event) => {
     if (event.target.type === 'checkbox') {
       return this.setState({ [event.target.id]: event.target.checked });
     }
-    this.setState({ [event.target.id]: event.target.value });
+    this.setState({ [event.target.id]: event.target.value }, this.validation);
   }
 
   render() {
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form { ...this.state } onInputChange={ this.onInputChange } />
-        <Card { ...this.state } onInputChange={ this.onInputChange } />
+        <Form
+          { ...this.state }
+          onInputChange={ this.onInputChange }
+        />
+        <Card
+          { ...this.state }
+          onInputChange={ this.onInputChange }
+        />
       </div>
     );
   }
